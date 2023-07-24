a6b3a6975b5996b1b2124039f3abb5945ea5f9a8
Aleksei Kachkin <lex6164665@yandex.ru>
2023-07-24 20:13:39 +0300

    refactor: move create garage in individual block 24/07/2023 20:13 Mon

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
 import { createMainButtons } from './modules/createMainButtons';
-import { createCarForm } from './modules/createCarForm';
-import { updateCarForm } from './modules/UpdateCarForm';
-import { renderControlButtons } from './modules/renderControlButtons';
 import './styles.css';
 import { getCars } from './modules/requests';
 
 const BODY = document.body;
 createMainButtons(BODY);
-createCarForm(BODY);
-updateCarForm(BODY);
-renderControlButtons(BODY);
-const MAIN = document.createElement('main');
-MAIN.classList.add('container');
-BODY.append(MAIN);
+const wrap = document.createElement('div');
+wrap.classList.add('wrap');
+BODY.append(wrap);
 getCars();
--- src/modules/garage.ts
+++ src/modules/garage.ts
@@ -1,15 +1,28 @@
 import { Garage } from '../types/types';
+import { createCarForm } from './createCarForm';
+import { updateCarForm } from './UpdateCarForm';
+import { renderControlButtons } from './renderControlButtons';
 import { carRender } from './carRender';
 export const garageRender = (data: Garage) => {
-    const MAIN = document.querySelector('.container') as HTMLElement;
-    MAIN.innerHTML = '';
+    const wrap = document.querySelector('.wrap') as HTMLElement;
+    createCarForm(wrap);
+    updateCarForm(wrap);
+    renderControlButtons(wrap);
+    let main = document.querySelector('.garage-container') as HTMLElement;
+    if (main !== null) {
+        main.innerHTML = '';
+    } else {
+        main = document.createElement('main');
+        main.classList.add('garage-container');
+        wrap.append(main);
+    }
     const TITLE = document.createElement('h1');
     TITLE.classList.add('garage-title');
     TITLE.textContent = `GARAGE (${data.length})`;
-    MAIN.append(TITLE);
+    main.append(TITLE);
     const GARAGE = document.createElement('div');
     GARAGE.classList.add('cars-container');
-    MAIN.append(GARAGE);
+    main.append(GARAGE);
 
     for (const car of data) {
         carRender(car, GARAGE);
